4a869e01f6e9f12bdbaba3ffd1e9ec14eb7af725
Michael Blasius <michaelblasius@Michaels-MacBook-Pro.local>
2015-11-11 18:41:42 -0700

    Added rerouting for invalid urls

--- server/routes.js
+++ server/routes.js
@@ -17,11 +17,15 @@ module.exports = function(app) {
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
+  	.get(errors[404]);
 
   // All other routes should redirect to the index.html
   app.route('/sessions')
     .get(function(req, res) {
       res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
     });
+
+  app.all('*', function(req, res){
+  	res.redirect('/sessions')
+  });
 };
