665f1a2e83bf1b75e925debffaddc0d82d827393
Michael Blasius <mblasiusnyc@gmail.com>
2015-11-13 13:57:11 -0700

    Refactored click-to-edit directive

--- client/app/directives/clickToEdit/clickToEdit.directive.js
+++ client/app/directives/clickToEdit/clickToEdit.directive.js
@@ -2,12 +2,6 @@
 
 angular.module('pokerTrackerApp')
   .directive('clickToEdit', function () {
-  	var templatePaths = {
-  		text: 'app/directives/clickToEdit/clickToEdit.text.html',
-  		dropdown: 'app/directives/clickToEdit/clickToEdit.dropdown.html',
-  		number: 'app/directives/clickToEdit/clickToEdit.number.html',
-  		date: 'app/directives/clickToEdit/clickToEdit.date.html',
-  	}
 
     return {
       template: '<div ng-include="templateUrl"></div>',
@@ -22,9 +16,6 @@ angular.module('pokerTrackerApp')
       		scope.templateUrl = 'app/directives/clickToEdit/clickToEdit.' + attrs.fieldType + '.html';
       },
       controller: function($scope) {
-      	$scope.blurCallback = function(){
-      		$scope.save();
-      	}
 
         $scope.view = {
           editableValue: $scope.value,
@@ -32,8 +23,8 @@ angular.module('pokerTrackerApp')
         };
 
         $scope.enableEditor = function() {
-          $scope.view.editorEnabled = true;
-          $scope.view.editableValue = $scope.value;
+	          $scope.view.editorEnabled = true;
+	          $scope.view.editableValue = $scope.value;
         };
 
         $scope.disableEditor = function() {
