ef126579ff71f4458804a1d3ed2fe093d10f9a13
Michael Blasius <mblasiusnyc@gmail.com>
2016-01-14 00:17:40 -0700

    Changed app.js

--- poker-tracker/www/js/app.js
+++ poker-tracker/www/js/app.js
@@ -11,60 +11,62 @@ angular.module('pokerTrackerApp', [
   'angularMoment'
 ])
 
-.config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
-	  // Ionic uses AngularUI Router which uses the concept of states
-	  // Learn more here: https://github.com/angular-ui/ui-router
-	  // Set up the various states which the app can be in.
-	  // Each state's controller can be found in controllers.js
-	  $stateProvider
-	  // setup an abstract state for the tabs directive
-	    .state('tab', {
-	    url: '/tab',
-	    abstract: true,
-	    templateUrl: 'templates/tabs.html'
-	  })
-	  // Each tab has its own nav history stack:
-	  .state('tab.sessions', {
-	    url: '/sessions',
-	    views: {
-	      'tab-sessions': {
-	        templateUrl: 'templates/tab-sessions.html',
-	        controller: 'SessionsCtrl'
-	      }
-	    }
-	  })
-	  .state('tab.chats', {
-	      url: '/chats',
-	      views: {
-	        'tab-chats': {
-	          templateUrl: 'templates/tab-chats.html',
-	          controller: 'ChatsCtrl'
-	        }
-	      }
-	    })
-	    .state('tab.chat-detail', {
-	      url: '/chats/:chatId',
-	      views: {
-	        'tab-chats': {
-	          templateUrl: 'templates/chat-detail.html',
-	          controller: 'ChatDetailCtrl'
-	        }
-	      }
-	    })
-	  .state('tab.account', {
-	    url: '/account',
-	    views: {
-	      'tab-account': {
-	        templateUrl: 'templates/tab-account.html',
-	        controller: 'AccountCtrl'
-	      }
-	    }
-	  });
-	  // if none of the above states are matched, use this as the fallback
-	  $urlRouterProvider.otherwise('/tab/sessions');
-    // $locationProvider.html5Mode(true);
-    $httpProvider.interceptors.push('authInterceptor');
-	})
+.config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, $ionicConfigProvider) {
+	$ionicConfigProvider.tabs.position('bottom')
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+  // Each tab has its own nav history stack:
+  .state('tab.sessions', {
+    url: '/sessions',
+    views: {
+      'tab-sessions': {
+        templateUrl: 'templates/tab-sessions.html',
+        controller: 'SessionsCtrl'
+      }
+    }
+  })
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/sessions');
+  // $locationProvider.html5Mode(true);
+  $httpProvider.interceptors.push('authInterceptor');
+})
 
   	// OLD STATES
     // $stateProvider
@@ -86,61 +88,61 @@ angular.module('pokerTrackerApp', [
     // $urlRouterProvider.otherwise('/sessions');
 
 
-  .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        cordova.plugins.Keyboard.disableScroll(true);
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
 
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
+  return {
+    // Add authorization token to headers
+    request: function (config) {
+      config.headers = config.headers || {};
+      if ($cookieStore.get('token')) {
+        config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
       }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
+      return config;
+    },
+
+    // Intercept 401s and redirect you to login
+    responseError: function(response) {
+      if(response.status === 401) {
+        $location.path('/login');
+        // remove any stale tokens
+        $cookieStore.remove('token');
+        return $q.reject(response);
       }
-    });
-  })
-
-  .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
-    return {
-      // Add authorization token to headers
-      request: function (config) {
-        config.headers = config.headers || {};
-        if ($cookieStore.get('token')) {
-          config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
-        }
-        return config;
-      },
-
-      // Intercept 401s and redirect you to login
-      responseError: function(response) {
-        if(response.status === 401) {
-          $location.path('/login');
-          // remove any stale tokens
-          $cookieStore.remove('token');
-          return $q.reject(response);
-        }
-        else {
-          return $q.reject(response);
-        }
+      else {
+        return $q.reject(response);
       }
-    };
-  })
+    }
+  };
+})
 
-  // .run(function ($rootScope, $location, Auth, amMoment) {
-  //   // Redirect to login if route requires auth and you're not logged in
-  //   $rootScope.$on('$stateChangeStart', function (event, next) {
-  //     Auth.isLoggedInAsync(function(loggedIn) {
-  //       if (next.authenticate && !loggedIn) {
-  //         event.preventDefault();
-  //         $location.path('/login');
-  //       }
-  //     });
-  //   });
-  //   amMoment.changeLocale('de');
-  // })
+// .run(function ($rootScope, $location, Auth, amMoment) {
+//   // Redirect to login if route requires auth and you're not logged in
+//   $rootScope.$on('$stateChangeStart', function (event, next) {
+//     Auth.isLoggedInAsync(function(loggedIn) {
+//       if (next.authenticate && !loggedIn) {
+//         event.preventDefault();
+//         $location.path('/login');
+//       }
+//     });
+//   });
+//   amMoment.changeLocale('de');
+// })
 
-  .run(function (amMoment) {
-    amMoment.changeLocale('de');
-   });
+.run(function (amMoment) {
+  amMoment.changeLocale('de');
+ });
